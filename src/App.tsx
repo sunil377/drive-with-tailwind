f7dacb0a0b60be2d080d25a1316512e954720d7a
sunil panwar <sunilpanwar1996@gmail.com>
2021-05-22 17:46:05 +0530

    trying do make thumbnail of images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { lazy, Suspense } from "react";
 import { Switch } from "react-router";
 
-
 import { AuthProvider } from "./Contexts/useAuthContext";
 
 import Navbar from "./Components/Navbar";
@@ -18,32 +17,33 @@ import Login from "./pages/Login";
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 
 export default function App() {
-    return (
-        <AuthProvider>
-            <ErrorBoundary>
-                <Navbar />
-                <Suspense fallback={<div>loading...</div>}>
-                    <Switch>
-                        <PublicRoute path="/login" component={Login} />
-                        <PublicRoute path="/signup" component={Signup} />
-                        <PrivateRoute
-                            path="/profile"
-                            exact
-                            component={Profile}
-                        />
-                        <PublicRoute
-                            path="/forgotpassword"
-                            component={ForgotPassword}
-                        />
-                        <PrivateRoute
-                            path="/folders/:id"
-                            exact
-                            component={Dashboard}
-                        />
-                        <PrivateRoute path="/" exact component={Dashboard} />
-                    </Switch>
-                </Suspense>
-            </ErrorBoundary>
-        </AuthProvider>
-    );
+	console.log(Dashboard);
+	return (
+		<AuthProvider>
+			<ErrorBoundary>
+				<Navbar />
+				<Suspense fallback={<div>loading...</div>}>
+					<Switch>
+						<PublicRoute path="/login" component={Login} />
+						<PublicRoute path="/signup" component={Signup} />
+						<PrivateRoute
+							path="/profile"
+							exact
+							component={Profile}
+						/>
+						<PublicRoute
+							path="/forgotpassword"
+							component={ForgotPassword}
+						/>
+						<PrivateRoute
+							path="/folders/:id"
+							exact
+							component={Dashboard}
+						/>
+						<PrivateRoute path="/" exact component={Dashboard} />
+					</Switch>
+				</Suspense>
+			</ErrorBoundary>
+		</AuthProvider>
+	);
 }
