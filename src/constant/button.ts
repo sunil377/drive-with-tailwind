ecc87ffc36996fe8891ea188b74c4efb4c6e7fac
sunil panwar <sunilpanwar1996@gmail.com>
2021-09-25 22:01:32 +0530

    fixed button.ts file

---
+++ src/constant/button.ts
@@ -0,0 +1,6 @@
+export const BTN = `px-4 py-2 rounded-sm border border-transparent 
+w-full block capitalize text-center dark:hover:border-transparent
+disabled:cursor-not-allowed disabled:opacity-50 
+transition duration-200 ease-linear  text-white 
+hover:bg-white dark:hover:bg-white disabled:text-white focus:bg-white
+focus:outline-none`;
